29a9555691c29b4ae77a6c4ae9d5c57822b7b9a4
Fabián Varela Bonett <jfabian.303@gmail.com>
2018-01-16 16:54:45 -0500

    Create the console project to SOLID Principles

---
+++ SOLIDPrinciplesConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLIDPrinciplesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
